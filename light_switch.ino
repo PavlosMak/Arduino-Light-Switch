9ec322c2c0fa737e1ab7399779c50413b3dded50
Pavlos Makridis <pavlosmakridis1322001@gmail.com>
2018-05-21 22:30:54 +0300

    Add files via upload

---
+++ light_switch.ino
@@ -0,0 +1,30 @@
+int sensorValue;
+int sensorLow = 0;
+int sensorHigh = 1023; //this will automatically change during the calibration loop
+const int led = 2; //the LED's pin
+
+void setup() {
+  pinMode(led, OUTPUT);
+  while (millis() < 500){ //calibration loop starts here 
+  	sensorValue = analogRead(A0);
+    if(sensorValue > sensorHigh){ 
+    	sensorHigh = sensorValue;
+    }
+    if(sensorValue < sensorLow){
+    	sensorLow = sensorValue;
+    }
+  } //calibration loop end here
+}
+
+void loop() {
+  sensorValue = analogRead(A0);
+  if(sensorValue == sensorHigh){
+  	digitalWrite(led, HIGH);
+  }
+  else if(sensorValue == sensorLow){
+  	digitalWrite(led, LOW);
+  }
+  else{
+  	digitalWrite(led, LOW);
+  }
+}
